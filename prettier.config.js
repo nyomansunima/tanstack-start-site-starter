d8eed75479e3e8f36ae753c4a328187ec932f35c
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-05 18:35:05 +0800

    refactor: use double quote instead

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 //  @ts-check
-
 /** @type {import('prettier').Config} */
 const config = {
   semi: false,
-  singleQuote: true,
+  singleQuote: false,
   trailingComma: "all",
-};
+  endOfLine: "auto",
+}
 
-export default config;
+export default config
