4c108fd9ae513b6397b4fcd92e17a85378918268
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-06 20:34:39 +0800

    refactor: add boilerplate code

--- src/shared/libs/shared-metadata.ts
+++ src/shared/libs/shared-metadata.ts
@@ -2,12 +2,15 @@ interface MetaTagsInput {
   title: string
   description: string
   image?: string
-  keyword?: string
+  keywords?: string
 }
 
-export function generateMetaTags(
-  meta: MetaTagsInput,
-):
+export function generateMetaTags({
+  title,
+  description,
+  image,
+  keywords,
+}: MetaTagsInput):
   | (
       | React.DetailedHTMLProps<
           React.MetaHTMLAttributes<HTMLMetaElement>,
@@ -18,11 +21,64 @@ export function generateMetaTags(
   | undefined {
   return [
     {
-      title: meta.title,
+      title,
     },
     {
       name: "description",
-      content: meta.description,
+      content: description,
+    },
+
+    {
+      name: "description",
+      content: description,
+    },
+    {
+      name: "keywords",
+      content:
+        keywords ||
+        "Product Designer, UI UX Designer, Web Designer, Shopify Developer, Web Developer, Webflow Expert, Framer Expert, Squarespace, Wix Developer, Fullstack, Mobile Developer, Indie Hacker",
+    },
+    { name: "creator", content: "nyomansunima" },
+    { name: "publisher", content: "nyomansunima" },
+    { name: "application-name", content: "Nyoman Sunima" },
+    { name: "google-site-verification", content: "" },
+    { name: "category", content: "Websites" },
+    { name: "generator", content: "React Router" },
+    { name: "pinterest-rich-pin", content: "true" },
+
+    {
+      name: "og:title",
+      content: title,
+    },
+    {
+      name: "og:description",
+      content: description,
+    },
+    {
+      name: "og:image",
+      content:
+        image ??
+        "https://cdn.hashnode.com/res/hashnode/image/upload/v1729709107977/6dabcb40-d18e-48cf-a86b-7b423efb6605.png",
+    },
+    { name: "og:locale", content: "en_US" },
+    { name: "og:type", content: "website" },
+
+    {
+      name: "twitter:title",
+      content: title,
+    },
+    {
+      name: "twitter:description",
+      content: description,
+    },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:site", content: "nyomansunima" },
+    { name: "twitter:creator", content: "@nyomansunima" },
+    {
+      name: "twitter:image",
+      content:
+        image ??
+        "https://cdn.hashnode.com/res/hashnode/image/upload/v1729709107977/6dabcb40-d18e-48cf-a86b-7b423efb6605.png",
     },
   ]
 }
