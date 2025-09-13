66a7b92e6f7f71145643552297421e4d0490fd8f
Nyoman Sunima <nyomansunima@gmail.com>
2025-09-14 00:37:31 +0800

    refactor: split the router and add slot for integration with context

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,11 +2,18 @@ import { createRouter as createTanstackRouter } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen"
 
 export const createRouter = () => {
-  return createTanstackRouter({
+  // TODO: Define your all integrations code
+  // with the router
+  //
+  // Define the router including with
+  // context, data and etc.
+  const router = createTanstackRouter({
     routeTree,
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   })
+
+  return router
 }
 
 declare module "@tanstack/react-router" {
